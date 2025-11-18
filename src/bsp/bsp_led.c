b8c216c923b62452e909835aa4dba0899054a66c
j-thd <36506376+j-thd@users.noreply.github.com>
2025-11-18 19:19:19 +0100

    Test colours at 30 degrees too. Clean up print statements.

--- src/bsp/bsp_led.c
+++ src/bsp/bsp_led.c
@@ -49,7 +49,7 @@ BSP_RGB_colour BSP_LED_RGB_from_HSL(SFP_11_20 hue, SFP_11_20 sat, SFP_11_20 ligh
         SFP_11_20_ONE  - abs((lightness<<1) - SFP_11_20_ONE),
          sat,
          5 ,5 );
-    JSM_PRINTF("Chroma: %i\n", chroma);
+    // JSM_PRINTF("Chroma: %i\n", chroma);
     J_ASSERT(chroma >=0);
     J_ASSERT(chroma <= SFP_11_20_ONE);
     // Hue dash should still be a float, so a constant of 1/60 is needed in
@@ -65,24 +65,24 @@ BSP_RGB_colour BSP_LED_RGB_from_HSL(SFP_11_20 hue, SFP_11_20 sat, SFP_11_20 ligh
         15,
         0
     );
-    JSM_PRINTF("Hue_dash: %i\n", hue_dash);
+    // JSM_PRINTF("Hue_dash: %i\n", hue_dash);
     J_ASSERT(0 <=  hue_dash);
     J_ASSERT(6*SFP_11_20_ONE >= hue_dash);
-    JSM_PRINTF("Y: %i\n", abs(SFP_11_20_MOD_2(hue_dash) - SFP_11_20_ONE));
+    //JSM_PRINTF("Y: %i\n", abs(SFP_11_20_MOD_2(hue_dash) - SFP_11_20_ONE));
     SFP_11_20 X = SFP_11_20_MULT(
         chroma,
         (SFP_11_20_ONE - abs(SFP_11_20_MOD_2(hue_dash) - SFP_11_20_ONE)),
         5,
         5
     );
-    JSM_PRINTF("X: %i\n", X);
+    // JSM_PRINTF("X: %i\n", X);
     J_ASSERT(X >=0);
     J_ASSERT(X <= SFP_11_20_ONE);
     // Calculate R_1, G_1, B_1 based on hue_dash.
     // Instead of doing 6 comparisons with floats, hue_dash can also be cast to
     // an int, instead.
     uint8_t hdi = SFP_11_20_TO_INT(hue_dash);
-    JSM_PRINTF("hdi: %i\n", hdi);
+    // JSM_PRINTF("hdi: %i\n", hdi);
     SFP_11_20 R,G,B;
 
     switch(hdi){
--- tests/tests_bsp_led.c
+++ tests/tests_bsp_led.c
@@ -166,5 +166,75 @@ JT_TEST("Test the rainbow!"){
     JT_ASSERT(res.rgb[2] == 255);
 }
 
+JT_TEST("Semi-arbitrary colour test"){
+    // Within each 60 degree section, test 1 colours with an arbitrary H,S,L
+    // value.
+    // In practice this means just picking things from the swatches on
+    // wikipedia:
+    // https://en.wikipedia.org/wiki/HSL_and_HSV#Swatches
+    // 1 out of 255 accuracy is fine.
+    
+
+
+    // 30 degrees.
+    SFP_11_20 H = FLOAT_TO_SFP_11_20(30.0f);
+    SFP_11_20 S = FLOAT_TO_SFP_11_20(0.75f);
+    SFP_11_20 L = FLOAT_TO_SFP_11_20(0.875f);
+    BSP_RGB_colour res = BSP_LED_RGB_from_HSL(H, S, L);
+    // RGB = F7DFC7
+    JT_ASSERT(res.rgb[0] >= 0xF6 && res.rgb[0] <= 0xF8);
+    JT_ASSERT(res.rgb[1] >= 0xDE && res.rgb[1] <= 0xE0);
+    JT_ASSERT(res.rgb[2] >= 0xC6 && res.rgb[2] <= 0xC8);
+
+    // 90 degrees.
+    H = FLOAT_TO_SFP_11_20(90.0f);
+    S = FLOAT_TO_SFP_11_20(0.25f);
+    L = FLOAT_TO_SFP_11_20(0.125f);
+    res = BSP_LED_RGB_from_HSL(H, S, L);
+    // RGB = 202818
+    JT_ASSERT(res.rgb[0] >= 0x19 && res.rgb[0] <= 0x21);
+    JT_ASSERT(res.rgb[1] >= 0x27 && res.rgb[1] <= 0x29);
+    JT_ASSERT(res.rgb[2] >= 0x17 && res.rgb[2] <= 0x19);
+
+    // 150 degrees.
+    H = FLOAT_TO_SFP_11_20(150.0f);
+    S = FLOAT_TO_SFP_11_20(1.0f);
+    L = FLOAT_TO_SFP_11_20(0.625f);
+    res = BSP_LED_RGB_from_HSL(H, S, L);
+    // RGB = 40FF9F
+    JT_ASSERT(res.rgb[0] >= 0x39 && res.rgb[0] <= 0x41);
+    JT_ASSERT(res.rgb[1] >= 0xFE && res.rgb[1] <= 0xFF);
+    JT_ASSERT(res.rgb[2] >= 0x9E && res.rgb[2] <= 0xA0);
+
+    // 210 degrees.
+    H = FLOAT_TO_SFP_11_20(210.0f);
+    S = FLOAT_TO_SFP_11_20(0.5f);
+    L = FLOAT_TO_SFP_11_20(0.875f);
+    res = BSP_LED_RGB_from_HSL(H, S, L);
+    // RGB = CFDFEF
+    JT_ASSERT(res.rgb[0] >= 0xCE && res.rgb[0] <= 0xD0);
+    JT_ASSERT(res.rgb[1] >= 0xDE && res.rgb[1] <= 0xE0);
+    JT_ASSERT(res.rgb[2] >= 0xEE && res.rgb[2] <= 0xF0);
+
+    // 270 degrees.
+    H = FLOAT_TO_SFP_11_20(270.0f);
+    S = FLOAT_TO_SFP_11_20(0.25f);
+    L = FLOAT_TO_SFP_11_20(0.375f);
+    res = BSP_LED_RGB_from_HSL(H, S, L);
+    // RGB = 604878
+    JT_ASSERT(res.rgb[0] >= 0x59 && res.rgb[0] <= 0x61);
+    JT_ASSERT(res.rgb[1] >= 0x47 && res.rgb[1] <= 0x49);
+    JT_ASSERT(res.rgb[2] >= 0x77 && res.rgb[2] <= 0x79);
+
+    // 330 degrees.
+    H = FLOAT_TO_SFP_11_20(330.0f);
+    S = FLOAT_TO_SFP_11_20(1.0f);
+    L = FLOAT_TO_SFP_11_20(0.375f);
+    res = BSP_LED_RGB_from_HSL(H, S, L);
+    // RGB = BF0060
+    JT_ASSERT(res.rgb[0] >= 0xBE && res.rgb[0] <= 0xC0);
+    JT_ASSERT(res.rgb[1] >= 0x00 && res.rgb[1] <= 0x01);
+    JT_ASSERT(res.rgb[2] >= 0x59 && res.rgb[2] <= 0x61);
+}
 
 }
